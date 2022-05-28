5cbf6055b1f1533ffec00338d78ed79751ec647d
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-05-28 11:38:47 +0300

    create grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,27 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="App-wrapper">
+            <header className='header'>
+                <div className='header__img'>
+                    <img src="https://cdn-icons-png.flaticon.com/512/841/841364.png" alt="logo"/>
+                </div>
+            </header>
+            <nav className='nav'>
+
+                    <div>Profile</div>
+                    <div>Messages</div>
+                    <div>News</div>
+                    <div>Music</div>
+                    <div>Settings</div>
+
+            </nav>
+            <body className='content'>
+                main content
+            </body>
+        </div>
+    );
 }
 
 export default App;
