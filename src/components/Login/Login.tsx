ad40c29a9a843b1c84135a3a989b1f377e86223e
grigorydobrenko <grigorydobrenko97@gmail.com>
2022-10-19 17:37:00 +0300

    add react-hook-form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import {SubmitHandler, useForm} from "react-hook-form";
+
+
+type FormData = {
+    login: string;
+    password: string;
+    rememberMe: boolean
+}
+
+const LoginForm = () => {
+    const {register, handleSubmit} = useForm<FormData>()
+
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        console.log(data)
+    }
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div><input type="text" {...register('login')}/></div>
+            <div><input type="text" {...register('password')}/></div>
+            <div><input type="checkbox" {...register('rememberMe')}/>remember me</div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    );
+}
+
 
 const Login = () => {
     return (
         <div>
-            LOGIN
+            <h1>Login</h1>
+            <LoginForm/>
         </div>
+
     );
 };
 
