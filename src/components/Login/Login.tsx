cf2f0073a6372e846aea8520c06f61df87604727
Grigory Dobrenko <grigorydobrenko97@gmail.com>
2022-11-21 17:48:00 +0300

    feat: add validators

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {SubmitHandler, useForm} from "react-hook-form";
+import {required} from "../../utils/validators/validators";
+import errorsStyles from "../Common/FormsControls/FormControls.module.css";
 
 
 type FormData = {
@@ -9,15 +11,29 @@ type FormData = {
 }
 
 const LoginForm = () => {
-    const {register, handleSubmit} = useForm<FormData>()
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<FormData>()
 
     const onSubmit: SubmitHandler<FormData> = (data) => {
         console.log(data)
+        reset()
     }
+
+    const validateObj = {
+        validate: {
+            required
+        }
+    }
+
+    const loginClassName = errors?.login ? errorsStyles.errorBorder : ''
+    const passwordClassName = errors?.password ? errorsStyles.errorBorder : ''
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <div><input type="text" {...register('login')}/></div>
-            <div><input type="text" {...register('password')}/></div>
+            <div><input type="text" {...register('login', validateObj)} className={`${errorsStyles.outlineNone} ${loginClassName}`}/>
+                {errors?.login && <div className={errorsStyles.errorMessageColor}>{errors.login.message}</div>}
+            </div>
+            <div><input type="text" {...register('password', validateObj)} className={`${errorsStyles.outlineNone} ${passwordClassName}`}/>
+                {errors?.password && <div className={errorsStyles.errorMessageColor}>{errors.password.message}</div>}</div>
             <div><input type="checkbox" {...register('rememberMe')}/>remember me</div>
             <div>
                 <button>Login</button>
